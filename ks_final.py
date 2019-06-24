667f822de1ffb90ee3fec742a449eeb16ef445cb
joshmnemonic <37618182+joshmnemonic@users.noreply.github.com>
2019-06-24 08:20:29 +0200

    Add files via upload

--- ks_final.py
+++ ks_final.py
@@ -1,10 +1,9 @@
-import os
+import os, smtplib
 
 dir_name = ("c:\\python_test\\")
 
 file_count = sum([len(files) for r, d, files in os.walk(dir_name)])
-if file_count > 3
 ks_folder = os.listdir(dir_name)
 for item in ks_folder:
-    if item.endswith(".txt"):
+    if item.endswith(".txt") and file_count > 3:
         os.remove(os.path.join(dir_name, item))
